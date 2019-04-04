e610c3e532c97d999a51e9e8489465eff1d972f2
Scott <spoiledtechie@gmail.com>
2019-04-03 22:58:08 -0400

    updated example

---
+++ WebApplication2/Controllers/TagCloudController.cs
@@ -0,0 +1,18 @@
+﻿namespace WebExample.Controllers
+{
+    using System.Web.Mvc;
+    using Sparc.TagCloud;
+    using WebApplication2.Properties;
+
+    public class TagCloudController : Controller
+    {
+        public ActionResult Index()
+        {
+            var phrases = new string[] { Resources.CloudInput};
+            var model = new TagCloudAnalyzer()
+                .ComputeTagCloud(phrases)
+                .Shuffle();
+            return View(model);
+        }
+    }
+}
